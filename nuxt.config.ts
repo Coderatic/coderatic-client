96d8633e056dd72399503735c091fa750242b853
Saad Ahmed <saadahmedcronos@gmail.com>
2023-07-19 23:45:12 +0500

    Add custom markdown stylesheets and qnotify plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.8/katex.min.css",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css",
+        },
+      ],
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
@@ -8,32 +22,32 @@ export default defineNuxtConfig({
     "nuxt-quasar-ui",
     "@nuxtjs/google-fonts",
   ],
+
   runtimeConfig: {
     public: {
-      APIBaseURL: process.env.BASE_URL || "http://localhost:5000",
+      APIBaseURL: process.env.API_BASE_URL || "http://localhost:5000",
+      NitroBaseURL: process.env.NITRO_BASE_URL || "http://localhost:3000",
     },
   },
-  imports: {
-    autoImport: true,
-  },
 
-  // Libs
   quasar: {
     iconSet: "fontawesome-v6",
+    plugins: ["Notify"],
     config: {
       brand: {
         primary: "#626ee3",
         secondary: "#e26a6a",
         accent: "#47b89c",
-
         dark: "#324466",
-        // @ts-ignore
-        "dark-page": "#212a3b",
-
         positive: "#21BA45",
-        negative: "#C10015",
+        negative: "#e06060",
         info: "#edc1bb",
         warning: "#F2C037",
+      },
+      notify: {
+        position: "top",
+        timeout: 2000,
+        textColor: "white",
       },
     },
     extras: {
