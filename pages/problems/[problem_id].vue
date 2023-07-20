9d433ea09a8b7b32fe147e013c396d08f2b5b7c2
Saad Ahmed <saadahmedcronos@gmail.com>
2023-07-20 19:39:10 +0500

    Reformat

--- pages/problems/[problem_id].vue
+++ pages/problems/[problem_id].vue
@@ -6,23 +6,12 @@
       <markdown-component class="mt-8" :content="problem_statement" />
     </div>
     <div class="w-1 h-full border-l border-gray-300 sm:hidden lg:block" />
-    <div
-      class="flex flex-row lg:w-[51%] sm:w-full h-screen gap-0 lg:mt-0 sm:mt-10"
-    >
+    <div class="flex flex-row lg:w-[51%] sm:w-full h-screen gap-0 lg:mt-0 sm:mt-10">
       <client-only>
-        <codemirror
-          v-model="code"
-          placeholder="Code goes here..."
-          :style="{ height: '90%', width: '100%' }"
-          :autofocus="true"
-          :indent-with-tab="true"
-          :tab-size="2"
-          :extensions="extensions"
-        />
+        <codemirror v-model="code" placeholder="Code goes here..." :style="{ height: '90%', width: '100%' }"
+          :autofocus="true" :indent-with-tab="true" :tab-size="2" :extensions="extensions" />
       </client-only>
-      <div
-        class="flex flex-row-reverse items-center w-full h-[10%] gap-5 pr-5 bg-slate-200"
-      >
+      <div class="flex flex-row-reverse items-center w-full h-[10%] gap-5 pr-5 bg-slate-200">
         <q-btn class="h-10" color="primary" label="Submit" icon="send" />
       </div>
     </div>
