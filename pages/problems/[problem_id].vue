78e959d414f4d9623970e1c0132a117bd4b6b697
Saad Ahmed <saadahmedcronos@gmail.com>
2023-07-21 15:18:49 +0500

    fix css

--- pages/problems/[problem_id].vue
+++ pages/problems/[problem_id].vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="flex h-screen gap-1 lg:flex-col sm:flex-row">
-    <div class="mt-5 lg:w-1/2 sm:w-full">
-      <h1 class="mb-3 lg:text-5xl sm:text-3xl">{{ problem_name }}</h1>
+  <div class="flex h-full gap-1 md:flex-row sm:flex-col">
+    <div class="mt-5 md:basis-1/2 sm:w-full">
+      <h1 class="mb-3 md:text-5xl sm:text-3xl">{{ problem_name }}</h1>
       <hr />
       <markdown-component class="mt-8" :content="problem_statement" />
     </div>
-    <div class="w-1 h-full border-l border-gray-300 sm:hidden lg:block" />
-    <div class="flex flex-row lg:w-[51%] sm:w-full h-screen gap-0 lg:mt-0 sm:mt-10">
-      <client-only>
-        <codemirror v-model="code" placeholder="Code goes here..." :style="{ height: '90%', width: '100%' }"
-          :autofocus="true" :indent-with-tab="true" :tab-size="2" :extensions="extensions" />
-      </client-only>
-      <div class="flex flex-row-reverse items-center w-full h-[10%] gap-5 pr-5 bg-slate-200">
-        <q-btn class="h-10" color="primary" label="Submit" icon="send" />
+    <div class="w-1 h-full border-l border-gray-300 sm:hidden md:block" />
+    <div class="flex flex-col md:flex-grow h-full md:mt-0 sm:mt-10">
+      <div
+        class="flex flex-row items-center justify-between pl-5 pr-5 w-full h-[10%] gap-5 bg-slate-200"
+      >
+        <q-btn class="h-10" color="primary" label="Submit" icon="mdi-send" />
+        <q-btn
+          class="h-10"
+          color="primary"
+          label="Run tests"
+          icon="mdi-restart"
+        />
+      </div>
+      <div class="w-full flex-grow">
+        <client-only>
+          <codemirror
+            v-model="code"
+            placeholder="Code goes here..."
+            :style="{ height: '100%', width: '100%' }"
+            :autofocus="true"
+            :indent-with-tab="true"
+            :tab-size="2"
+            :extensions="extensions"
+          />
+        </client-only>
+      </div>
+      <div
+        class="flex flex-row-reverse items-center w-full h-[10%] gap-5 pr-5 bg-slate-200"
+      >
+        <q-btn class="h-10" color="primary" label="Submit" icon="mdi-send" />
+        <q-btn
+          class="h-10"
+          color="primary"
+          label="Run tests"
+          icon="mdi-restart"
+        />
       </div>
     </div>
   </div>
