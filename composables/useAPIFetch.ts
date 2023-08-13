e3c5af3e47dc6d2549db4569e01e301feef9ac42
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 20:36:20 +0500

    Use a promise based approach for error handling from the API

--- composables/useAPIFetch.ts
+++ composables/useAPIFetch.ts
@@ -1,9 +1,19 @@
-import { useFetch } from "#app";
+import { UseFetchOptions, useFetch } from "#app";
 
 type useFetchType = typeof useFetch;
 
-export const useAPIFetch: useFetchType = (path, options = {}) => {
-  const config = useRuntimeConfig();
-  options.baseURL = config.public.APIBaseURL;
-  return useFetch(path, options);
+export const useAPIFetch = async (
+	path: string,
+	options: UseFetchOptions<any, any>
+) => {
+	const config = useRuntimeConfig();
+	options.baseURL = config.public.APIBaseURL;
+
+	const { data, error } = await useFetch(path, options).catch((error) =>
+		Promise.reject(error)
+	);
+	if (error.value) {
+		return Promise.reject(error.value);
+	}
+	return Promise.resolve(data.value);
 };
