317e5cf4871387692896f9b4e3f79b3fa0010092
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 20:37:12 +0500

    Remove unused import

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,8 @@
 import { useUserStore } from "../store/auth.js";
-import token_check from "./token-check.js";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const userStore = useUserStore();
-  if (!userStore.user) {
-    return navigateTo("/auth/login");
-  }
+	const userStore = useUserStore();
+	if (!userStore.user) {
+		return navigateTo("/auth/login");
+	}
 });
