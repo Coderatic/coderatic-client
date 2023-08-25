a0929f6943c6ba7f640cd6c5b6d016fae504d951
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-25 22:30:37 +0500

    Remove unused log

--- middleware/token-check.global.ts
+++ middleware/token-check.global.ts
@@ -3,6 +3,5 @@ import { useUserStore } from "../store/auth.js";
 export default defineNuxtRouteMiddleware(async (to, from) => {
 	const userStore = useUserStore();
 	if (to.path.includes("/auth/account/OAuth")) return;
-	console.log("token check");
 	userStore.verifyToken();
 });
