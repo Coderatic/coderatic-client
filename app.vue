e437b08c17eb089f5ce670a4109be7277a9d09e3
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-25 22:26:43 +0500

    Add app level styling for dark mode

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
+</template>
+
+<style>
+#app {
+	font-family: "Lato", sans-serif;
+}
+
+body.body--dark {
+	@apply text-white bg-dark;
+}
+</style>
