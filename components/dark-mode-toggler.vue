32a1f5fad6c45a8b1cf0f31cc3c8663082d359ff
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-30 16:27:34 +0500

    Add file

---
+++ components/dark-mode-toggler.vue
@@ -0,0 +1,24 @@
+<template>
+	<q-toggle
+		v-model="theme"
+		color="secondary"
+		keep-color
+		checked-icon="mdi-white-balance-sunny"
+		unchecked-icon="mdi-moon-waning-crescent"
+		:toggle-indeterminate="false"
+		@click="changeMode"
+	/>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+const $q = useQuasar();
+
+const theme = ref(!$q.dark.isActive);
+colorMode.preference = $q.dark.isActive ? "dark" : "light";
+
+const changeMode = () => {
+	colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+	$q.dark.toggle();
+};
+</script>
