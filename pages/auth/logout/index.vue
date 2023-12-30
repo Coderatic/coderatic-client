ad869c9d7ae4bc0ade644e5e3b8165ce710d5d73
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-30 16:28:42 +0500

    Add file

---
+++ pages/auth/logout/index.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useUserStore } from "../../../store/auth.js";
+import notifyUser from "../../../composables/notifyUser.js";
+
+const userStore = useUserStore();
+onMounted(async () => {
+	await userStore.logOut();
+	notifyUser({
+		type: "positive",
+		message: "Logged out successfully",
+	});
+	await navigateTo("/");
+});
+</script>
