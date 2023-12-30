2395a0a4de3aab00ff923f76da2cfbce4b53141b
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-30 16:27:45 +0500

    Add file

---
+++ components/problem-card.vue
@@ -0,0 +1,18 @@
+<template>
+	<slot name="header" />
+	<div
+		class="flex flex-row min-h-[60px] min-w-full items-center justify-between p-4"
+		flat
+	>
+		<span class="text-lg font-medium">{{ props?.problem?.name }}</span>
+		<router-link :to="`/problems/${problem.slug}`">
+			<q-btn label="Solve" color="accent" class="q-px-xl" />
+		</router-link>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	problem: {},
+});
+</script>
